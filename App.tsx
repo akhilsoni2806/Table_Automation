3ed46efa27135cc85dcb312858ce6134316ef1ad
akhilsoni2806 <akhilsoni2806@gmail.com>
2025-12-07 13:46:01 -0500

    App.tsx

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Careers from './pages/Careers';
+import Contact from './pages/Contact';
+import AIChatWidget from './components/AIChatWidget';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+      <AIChatWidget />
+    </Router>
+  );
+};
+
+export default App;
